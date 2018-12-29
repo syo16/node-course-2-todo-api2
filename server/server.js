19cf784863cedb9694fd06e6201d2f16467cc10d
大崎祥一郎 <syo16_omt_happy@outlook.jp>
2018-12-29 20:31:24 +0900

    Add GET /todo/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+var {ObjectID} = require('mongodb');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -26,6 +27,25 @@ app.get('/todos', (req, res) => {
         res.send({todos});
     }, (e) => {
         res.status(400).send(e);
+    });
+});
+
+// GET /todos/1234567
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
     });
 });
 
